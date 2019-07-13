dc7b7f2d1c77b47fa88954f7177ff548cb1464d0
mcontoor <mcontoor@gmail.com>
2019-07-13 12:41:46 +0530

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>The App component</h1>
+      <Header />
       <Contact />
     </div>
   );

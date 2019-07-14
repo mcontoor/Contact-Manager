59ae95d376655001e002c9dd570c1a7cb343324a
mcontoor <mcontoor@gmail.com>
2019-07-14 15:09:17 +0530

    update components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Contact />
+      <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+      <Contact
+        name="Karen Smith"
+        email="karen@gmail.com"
+        phone="333-333-3333"
+      />
     </div>
   );
 }

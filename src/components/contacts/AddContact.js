4d8596498b989c33b219428ef611db620c472caa
mcontoor <mcontoor@gmail.com>
2019-07-19 17:55:36 +0530

    error checking and display

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import { Consumer } from "../../context";
+import uuid from "uuid";
+import TextInputGroup from "../layout/TextInputGroup";
+
+export default class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: "",
+    errors: {}
+  };
+
+  onSubmit = (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone, errors } = this.state;
+
+    //check for errors
+    if (name === "") {
+      this.setState({
+        errors: { name: "Name is required" }
+      });
+      return;
+    }
+
+    if (email === "") {
+      this.setState({
+        errors: { email: "Email is required" }
+      });
+      return;
+    }
+
+    if (phone === "") {
+      this.setState({
+        errors: { phone: "Phone is required" }
+      });
+      return;
+    }
+
+    const newContant = {
+      id: uuid(),
+      name,
+      email,
+      phone
+    };
+
+    dispatch({ type: "ADD_CONTACT", payload: newContant });
+
+    //clear state
+    this.setState({
+      name: "",
+      email: "",
+      phone: ""
+    });
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={this.onChange}
+                    errors={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    placeholder="Enter Email"
+                    value={email}
+                    onChange={this.onChange}
+                    errors={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter Phone"
+                    value={phone}
+                    onChange={this.onChange}
+                    errors={errors.phone}
+                  />
+
+                  <input
+                    type="submit"
+                    value="Add contact"
+                    className="btn btn-light btn-block"
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Consumer } from "../../context";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
+  onShowClick = () => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
+  };
+
+  render() {
+    const { contact } = this.props;
+    const { showContactInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {contact.name}{" "}
+                <i
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fas fa-times"
+                  onClick={this.onDeleteClick.bind(this, contact.id, dispatch)}
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {contact.email}</li>
+                  <li className="list-group-item">Phone: {contact.phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;

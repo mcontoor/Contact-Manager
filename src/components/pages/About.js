e2050a43b7ce22bd42a4c413937ce2b76fd2079a
mcontoor <mcontoor@gmail.com>
2019-07-20 23:51:28 +0530

    add page not found component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1 className="display-4">About Contact Manager</h1>
+      <h1 className="display-4">{this.props.match.params.id}</h1>
       <p className="lead">Simple app to manage contacts</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
